d8a4b870730d64c9a28d99dd23fd5961c049a13c
SaraSS02 <s285050@studenti.polito.it>
2024-06-09 01:05:36 +0200

    klynt

--- miniPlayerData.js
+++ miniPlayerData.js
@@ -3,13 +3,13 @@ klynt.miniPlayerData = {
     "title": "ARG",
     "thanksForWatchingWording": "Thanks for watching!",
     "description": "",
+    "launchAppWording": "Then Launch Project",
     "resumePlaybackWording": "Resume playback?",
     "redirectToMobileApp": "always",
     "yesWording": "Yes",
     "url": "",
     "noWording": "No",
-    "thumbnail": "Medias/Photos/PHOTO-2024-05-21-13-17-32.jpg",
+    "thumbnail": "Player/css/editor/img/mini-player.jpg",
     "analyticsKey": "",
-    "downloadAppWording": "Download App",
-    "launchAppWording": "Then Launch Project"
+    "downloadAppWording": "Download App"
 }
