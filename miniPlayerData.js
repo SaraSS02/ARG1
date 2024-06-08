f0bb744c835eb0bc4607867648654f70b9d224f7
SaraSS02 <s285050@studenti.polito.it>
2024-06-09 00:11:43 +0200

    klynt

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "title": "ARG",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "description": "",
+    "resumePlaybackWording": "Resume playback?",
+    "redirectToMobileApp": "always",
+    "yesWording": "Yes",
+    "url": "",
+    "noWording": "No",
+    "thumbnail": "Medias/Photos/PHOTO-2024-05-21-13-17-32.jpg",
+    "analyticsKey": "",
+    "downloadAppWording": "Download App",
+    "launchAppWording": "Then Launch Project"
+}
